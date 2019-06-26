17dfb4bc97f7d16e9fc25386ad0cb906dc982c79
sahij kaur <sahijpalkaur@gmail.com>
2019-06-26 12:01:23 -0400

    4th Lab and 4 commit

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(int? a)
         {
             return View();
         }
